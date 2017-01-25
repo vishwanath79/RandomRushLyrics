d68f83d264ad90b73fa1036aa2f3dab6494c5446
Subramanian <vishwanath79@outlook.com>
2017-01-24 23:52:29 -0800

    temp version

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
-import os
 import random
+import colorlog
 import requests
 from bs4 import BeautifulSoup
 from flask import Flask, render_template
@@ -7,7 +7,7 @@ from flask import Flask, render_template
 import creds
 
 app = Flask(__name__)
-
+logger = colorlog.getLogger()
 
 class Song:
     def __init__(self, title, artist):
@@ -18,29 +18,28 @@ class Song:
 def track_search(title, artist):
     url = creds.generate_url('track_search')
     payload = {'apikey': creds.api_key(), 'q_track': title, 'q_artist': artist, 'has_lyrics': 1}
+    logger.info("Track search")
     return requests.get(url, params=payload).json()
 
 
 def track_lyrics_get(track_id):
     url = creds.generate_url('track_lyrics_get')
     payload = {'apikey': creds.api_key(), 'track_id': str(track_id)}
+    logger.info("Get lyrics")
     return requests.get(url, params=payload).json()
 
 
 def randomizer(tracks, title, artist):
     print(title.upper(), "------", artist.upper())
     for track in tracks:
-        # get json response from MusiXmatch search for track
+        logger.info("Get json response from MusiXmatch search for track")
         response = track_search(track.title, track.artist)
         urls = []
         # get urls
         for i in response['message']['body']['track_list']:
             urls.append((i['track']['track_share_url']))
         # print(urls[0],"\n")
-
-
-
-        # pull out track_ids
+        logger.info(" pull out track_ids")
         track_ids = []
         for each in response['message']['body']['track_list']:
             if each['track']['has_lyrics'] == 1:
@@ -58,9 +57,10 @@ def randomizer(tracks, title, artist):
     return lyr, loneurl
 
 
-# GET ALL
-@app.route('/rushrandom')
+
+@app.route('/')
 def run_app():
+    logger.info("Get songlist from wikipedia")
     source_code = requests.get('https://en.wikipedia.org/wiki/List_of_songs_recorded_by_Rush')
     soup = BeautifulSoup(source_code.content, "lxml")
     songsdb = []
@@ -70,7 +70,7 @@ def run_app():
         if len(cells) == 5:
             songs = cells[0].find(text=True)
             songsdb.append(songs)
-            # print(songsdb)
+            print(songsdb)
 
     for x in range(0, 1):
         # try 4 times
@@ -86,7 +86,14 @@ def run_app():
             render_template("Error.html", error=str(e))
 
 
+
 if __name__ == "__main__":
-    server = '127.0.0.1'
-    port = int(os.environ.get('PORT', 8001))
-    app.run(host=server, port=port, debug=True)
+    # server = '127.0.0.1'
+    # port = int(os.environ.get('PORT', 8001))
+    # app.run(host=server, debug=True)
+    logger.setLevel(colorlog.colorlog.logging.INFO)
+    handler = colorlog.StreamHandler()
+    handler.setFormatter(colorlog.ColoredFormatter())
+    logger.addHandler(handler)
+    app.run()
+
